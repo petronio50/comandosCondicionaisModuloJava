a26e204d256bb4ebb220a7a1ef9aa722478d7cf9
petronio50 <136655989+petronio50@users.noreply.github.com>
2023-10-03 16:30:30 -0300

    Update ProgramaComadosIfElse.java

--- ProgramaComadosIfElse.java
+++ ProgramaComadosIfElse.java
@@ -1,7 +1,14 @@
 package comandoscondicionais;
-
+/*
+If/else
+O if/else é uma estrutura de condição em que uma expressão booleana é analisada. 
+Quando a condição que estiver dentro do if for verdadeira, ela é executada. 
+Já o else é utilizado para definir o que é executado quando a condição analisada pelo if for falsa. 
+Caso o if seja verdadeiro e, consequentemente executado, o else não é executado.
+O if pode ser utilizado em conjunto com o else ou até mesmo sozinho, caso necessário.
+Bibliografia- https://www.treinaweb.com.br/blog/estruturas-condicionais-e-estruturas-de-repeticao-em-java
+*/
 public class ProgramaComadosIfElse {
-
 	public static void main(String[] args) {
 		int a = 2;
 		int b = 4;
